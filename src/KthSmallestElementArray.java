a2df380b7c33cd7db10ed5f0558c8cc093bb8f69
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-27 22:49:51 -0700

    coded a method to find the kth smallest elelment, basically we need to get a max heap and keep only the k elements in it. then get the maximum element out of those k elements, which will be the kth smallest element in the array

--- src/KthSmallestElementArray.java
+++ src/KthSmallestElementArray.java
@@ -1,5 +1,19 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class KthSmallestElementArray {
+
+    public static int kthSmallestElementArray(int [] arr, int k){
+        PriorityQueue<Integer> ans = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i= 0; i<arr.length; i++){
+            ans.add(arr[i]);
+        }
+        while(ans.size()>k){
+            ans.poll();
+        }
+        return ans.peek();
+    }
     public static void main(String[] args) {
-        
+
     }
 }
